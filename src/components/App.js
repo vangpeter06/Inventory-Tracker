43d8052ad9259be945f5477bc26288f53df86a58
Peter Vang <vang_peter06@yahoo.com>
2022-09-09 09:58:34 -0700

    update app.js and add header

---
+++ src/components/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import Header from ",/Header";
+
+function App() {      
+  return (
+   <React.Fragment>
+    <Header/>
+    <CoffeeControl/>
+   </React.Fragment>
+  );
+}
+
+export default App;
---
+++ src/components/Header.js
@@ -0,0 +1,9 @@
+import React from "react";
+
+function Header () {
+  return (
+    <h1>Local Business Inventory Tracker </h1>
+  );
+}
+
+export default Header;
